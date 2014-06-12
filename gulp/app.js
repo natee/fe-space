84e5b38afe13dd29f02d353304d108da0fab5585
natee <realfalsezk@gmail.com>
2014-06-12 16:37:10 +0800

    update

--- gulp/app.js
+++ gulp/app.js
@@ -1,8 +1,4 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -17,22 +13,11 @@ app.engine('.html',ejs.__express);
 app.set('view engine', 'html');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,27 +1,55 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var path = require('path');
-var concat = require('gulp-concat');
+var less = require('gulp-less');//LESS
+var minifyJS = require('gulp-uglify');//压缩JS
+var minifyCSS = require('gulp-minify-css');//压缩CSS
+var concat = require('gulp-concat');//合并
 
-//less编译
-gulp.task('less', function () {
-    gulp.src('src/css/*.less')
+var paths = {
+    src_js: 'src/js/*.js',
+    src_css: 'src/css/*.less',
+    dist_js:'dist/js',
+    dist_css:'dist/css'
+};
+
+/**
+ * 源文件目录为/src/css/*.less
+ * 先通过gulp-less把.less文件编译成.css文件
+ * 通过gulp-minify-css把.css文件压缩
+ * 再通过gulp-concat把压缩后的.css文件合并到all.min.css中
+ * 发布到/dist/css目录下
+ */
+gulp.task('css', function() {
+    gulp.src(paths.src_css)
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(paths.dist_css))
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest(paths.dist_css))
 });
 
-//合并css
-gulp.task('css', function() {
-    gulp.src(['dist/css/style.css'])
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('dist/js'))
-});
-
-//合并scripts
+/**
+ * 源文件目录为/src/js/*.js
+ * 通过gulp-uglify把.js文件压缩
+ * 再通过gulp-concat把压缩后的.js文件合并到all.min.js中
+ * 发布到/dist/js目录下
+ */
 gulp.task('scripts', function() {
-    gulp.src(['src/js/file3.js', 'src/js/file1.js', 'src/js/file2.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist/js'))
+    gulp.src(paths.src_js)
+        .pipe(minifyJS())
+        .pipe(gulp.dest(paths.dist_js))
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest(paths.dist_js))
 });
+
+/**
+ * 监听文件变化，执行相应的任务
+ */
+gulp.task('watch', function() {
+    gulp.watch(paths.src_js, ['scripts']);
+    gulp.watch(paths.src_css, ['css']);
+});
+
+gulp.task('default', ['scripts', 'css', 'watch']);
